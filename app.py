df68b37475fada4d5383d8c56be9dda99fbee87a
trprince21 <trprince21@g.ucla.edu>
2023-06-10 18:26:45 -0700

    edited the app.py file

--- app.py
+++ app.py
@@ -1,57 +1,66 @@
-import streamlit as st
+import numpy as np
 import pandas as pd
+import streamlit as st
 from matplotlib import pyplot as plt
-import io
-
-web_apps = st.sidebar.selectbox("Select Web Apps",
-                                ("Exploratory Data Analysis", "Distributions"))
+import seaborn as sns
 
 
-if web_apps == "Exploratory Data Analysis":
+st.title("Exploratory Data Analysis")
 
-  uploaded_file = st.sidebar.file_uploader("Choose a file")
+# File uploader button:
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
 
-  if uploaded_file is not None:
-    # Can be used wherever a "file-like" object is accepted:
-    df = pd.read_csv(uploaded_file)
-    show_df = st.checkbox("Show Data Frame", key="disabled")
 
-    if show_df:
-      st.write(df)
+  # Relevant statistics about the dataset:
+  st.write("Number of rows:", df.shape[0])
+  st.write("Number of columns:", df.shape[1])
 
-    column_type = st.sidebar.selectbox('Select Data Type',
-                                       ("Numerical", "Categorical", "Bool", "Date"))
+  num_categorical = len(df.select_dtypes(include=['object', 'category']).columns)
+  st.write("Number of categorical variables:", num_categorical)
 
-    if column_type == "Numerical":
-      numerical_column = st.sidebar.selectbox(
-          'Select a Column', df.select_dtypes(include=['int64', 'float64']).columns)
+  num_numerical = len(df.select_dtypes(include=['int64', 'float64']).columns)
+  st.write("Number of numerical variables:", num_numerical)
 
-      # histogram
-      choose_color = st.color_picker('Pick a Color', "#69b3a2")
-      choose_opacity = st.slider(
-          'Color Opacity', min_value=0.0, max_value=1.0, step=0.05)
+  num_boolean = len(df.select_dtypes(include=['bool']).columns)
+  st.write("Number of boolean variables:", num_boolean)
 
-      hist_bins = st.slider('Number of bins', min_value=5,
-                            max_value=150, value=30)
-      hist_title = st.text_input('Set Title', 'Histogram')
-      hist_xtitle = st.text_input('Set x-axis Title', numerical_column)
+  # Input widget to select a single column from the dataset:
+  column = st.sidebar.selectbox('Select a Column', df.columns)
 
-      fig, ax = plt.subplots()
-      ax.hist(df[numerical_column], bins=hist_bins,
-              edgecolor="black", color=choose_color, alpha=choose_opacity)
-      ax.set_title(hist_title)
-      ax.set_xlabel(hist_xtitle)
-      ax.set_ylabel('Count')
+  if df[column].dtype in [np.int64, np.float64]:
+    numerical_column = column
+    
+    # Five-number summary in a table:
+    summary = df[numerical_column].describe()
+    st.write("Five-number summary:")
+    st.table(summary)
 
-      st.pyplot(fig)
-      filename = "plot.png"
-      fig.savefig(filename,dpi = 300)
+    # Distribution plot:
+    fig, ax = plt.subplots()
+    sns.histplot(x = df[numerical_column], 
+                 stat = "density", kde = True)
+    plt.title(f"Histogram with KDE curve of {numerical_column}")
+    plt.xlabel(numerical_column)
+    plt.ylabel("Density")
+    st.pyplot(fig)
 
-      # Display the download button
-      with open("plot.png", "rb") as file:
-        btn = st.download_button(
-            label="Download image",
-            data=file,
-            file_name="flower.png",
-            mime="image/png"
-        )
+
+  elif df[column].dtype in [object, pd.Categorical]:
+    categorical_column = column
+
+    # Proportions of each category level in a table:
+    proportions = df[categorical_column].value_counts(normalize=True).reset_index()
+    proportions.columns = [categorical_column, 'Proportion']
+    st.write("Proportions of each category level:")
+    st.table(proportions)
+
+    # Customized barplot:
+    fig, ax = plt.subplots()
+    sns.barplot(data = proportions, x = categorical_column, 
+                y = 'Proportion')
+    plt.title(f"Proportions of {categorical_column}")
+    plt.xlabel(categorical_column)
+    plt.ylabel("Proportion")
+    st.pyplot(fig)
